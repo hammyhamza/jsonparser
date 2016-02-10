ce19b3e8042d54087629db98f3e71a11e798c155
hamza <hamza.iosdev@gmail.com>
2016-02-10 15:04:30 +0530

    Json Parsed

--- JSONCall/ViewController.m
+++ JSONCall/ViewController.m
@@ -13,9 +13,11 @@
 @end
 
 @implementation ViewController
+@synthesize jsonTextView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +25,45 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(IBAction)getJSONFromURL:(id)sender{
+    
+    NSString *URLString = @"http://date.jsontest.com/";
+    NSURL *url = [NSURL URLWithString:URLString];
+
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    // Instantiate a session object.
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    
+    // Create a data task object to perform the data downloading.
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        if (error != nil) {
+            // If any error occurs then just display its description on the console.
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else{
+            NSError *error;
+            NSMutableDictionary *returnedDict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+            
+            if (error != nil) {
+                NSLog(@"%@", [error localizedDescription]);
+            }else{
+                NSLog(@"%@",returnedDict);
+                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                    jsonTextView.text = returnedDict.debugDescription;
+                }];
+
+            }
+
+        }
+    }];
+    
+    // Resume the task.
+    [task resume];
+
+}
+
 
 @end
